ef922f134d6919fbeb4bf94843d8067b1dfb0ec4
senateur-013 <99352816+senateur-013@users.noreply.github.com>
2025-12-01 22:51:08 +0100

    Update nuees_dynamiques.py

--- nuees_dynamiques.py
+++ nuees_dynamiques.py
@@ -1,1 +1,60 @@
+import numpy as np
+
+class NueesDynamiques:
+    def __init__(self, K, ni, max_iter=50, tol=1e-3):
+        self.K = K
+        self.ni = ni
+        self.max_iter = max_iter
+        self.tol = tol
+
+    def initialisation(self, X):
+        n = X.shape[0]
+        indices = np.random.permutation(n)
+        self.E = []
+        pos = 0
+        for _ in range(self.K):
+            self.E.append(X[indices[pos:pos+self.ni]])
+            pos += self.ni
+
+    def distance(self, x, Ei):
+        return np.mean(np.linalg.norm(Ei - x, axis=1))
+
+    def affectation(self, X):
+        classes = [[] for _ in range(self.K)]
+        for x in X:
+            distances = [self.distance(x, Ei) for Ei in self.E]
+            idx = np.argmin(distances)
+            classes[idx].append(x)
+        return classes
+
+    def nouvelle_nuee(self, Ci):
+        Ci = np.array(Ci)
+        centre = Ci.mean(axis=0)
+        distances = np.linalg.norm(Ci - centre, axis=1)
+        idx = np.argsort(distances)
+        return Ci[idx[:self.ni]]
+
+    def fit(self, X):
+        self.initialisation(X)
+
+        for _ in range(self.max_iter):
+            classes = self.affectation(X)
+            nouvelle_E = []
+
+            for i in range(self.K):
+                if len(classes[i]) >= self.ni:
+                    nouvelle_E.append(self.nouvelle_nuee(classes[i]))
+                else:
+                    nouvelle_E.append(self.E[i])
+
+            variation = sum(np.linalg.norm(self.E[i] - nouvelle_E[i])
+                            for i in range(self.K))
+
+            self.E = nouvelle_E
+
+            if variation < self.tol:
+                break
+
+        self.C = classes
+        return classes
 
